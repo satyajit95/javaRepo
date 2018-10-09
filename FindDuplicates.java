d991d0b92297a2de26d2a0b27cf4b5bd4b5d17ef
satyajit95 <43992349+satyajit95@users.noreply.github.com>
2018-10-09 18:13:09 +0530

    .kjshduyg
    kgifauyjkfsa

---
+++ FindDuplicates.java
@@ -0,0 +1,18 @@
+package com.nt.string;
+
+public class FindDuplicates {
+	int x,y;
+	public int add(int x,int y) {
+		return x+y;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		FindDuplicates fd=new FindDuplicates();
+		fd.add(10,15);
+		System.out.println(fd);
+
+	}
+
+}
